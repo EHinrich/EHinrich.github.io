eba03e6ff290a9063565a66097019ba2fbac06c6
EHinrich <90458838+EHinrich@users.noreply.github.com>
2021-10-12 11:59:53 +0300

    Add files via upload

---
+++ Калькулятор.js
@@ -0,0 +1,17 @@
+    function click1() {
+      let f1 = document.getElementsByName("field1");
+      let f2 = document.getElementsByName("field2");
+      let r = document.getElementById("result");
+      let re=/^\s*\d+\s*$/
+      if (re.exec(f1[0].value)&&(re.exec(f2[0].value)))
+      alert(f1[0].value*f2[0].value+" руб.");
+      else alert("Некорректный ввод");
+      return false;
+    }
+
+      
+      window.addEventListener('DOMContentLoaded', function (event) {
+        console.log("DOM fully loaded and parsed");
+        let b = document.getElementById("button");
+        b.addEventListener("click", click1);
+      });
